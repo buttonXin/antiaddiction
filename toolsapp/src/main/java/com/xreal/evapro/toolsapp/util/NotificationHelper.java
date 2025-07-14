3c2cc8e925c156c45391db1d80cfdbbb59c64c38
ggxin <ggxin@nreal.ai>
2025-07-14 14:39:55 +0800

    1.5.3 : feat: 通知改为大小通知.

--- toolsapp/src/main/java/com/xreal/evapro/toolsapp/util/NotificationHelper.java
+++ toolsapp/src/main/java/com/xreal/evapro/toolsapp/util/NotificationHelper.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.RemoteViews;
 
 import com.xreal.evapro.toolsapp.App;
 import com.xreal.evapro.toolsapp.R;
@@ -31,6 +32,8 @@ public class NotificationHelper {
     public static final int DELAY_TIME = 10 * 1000;
     public static final String HIDE_INFO = "hide";
     private final Runnable mDelayRunnable = () -> changeContent(HIDE_INFO);
+    private RemoteViews mBigView;
+    private RemoteViews mSmallView;
 
     private NotificationHelper() {
     }
@@ -66,10 +69,16 @@ public class NotificationHelper {
                 context, 0, new Intent(context, NoteAct.class), PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
         // 🔔 **构建通知**
+        mSmallView = new RemoteViews(context.getPackageName(), R.layout.notification_small);
+        mBigView = new RemoteViews(context.getPackageName(), R.layout.notification_big);
+        mSmallView.setTextViewText(R.id.notification_title, content);
+
 
         Notification.Builder builder = new Notification.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.mipmap.ic_launcher_round)  // 通知图标
-                .setContentTitle("标题")  // 标题
+//                .setContentTitle("标题")  // 标题
+                .setCustomContentView(mSmallView)
+                .setCustomBigContentView(mBigView)
 //                .setContentText(content)  // 内容
                 .setAutoCancel(true)  // 点击后自动取消通知
                 .setPriority(Notification.PRIORITY_LOW)  // 设置通知优先级为最低
@@ -91,23 +100,25 @@ public class NotificationHelper {
             return;
         }
 
+
         // hide时,点击显示内容
-        if (content.equals(HIDE_INFO)) {
-            PendingIntent pendingIntent = PendingIntent.getActivity(
-                    mContext, 0, new Intent(mContext, HideAct.class),
-                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-            notificationBuilder.setContentIntent(pendingIntent);
-        } else {
-            mHandler.removeCallbacks(mDelayRunnable);
-            PendingIntent pendingIntent = PendingIntent.getActivity(
-                    mContext, 0, new Intent(mContext, NoteAct.class),
-                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-            notificationBuilder.setContentIntent(pendingIntent);
-//            mHandler.postDelayed(mDelayRunnable, DELAY_TIME);
-            startCountdownWithAlarm(mContext, DELAY_TIME);
-        }
+//        if (content.equals(HIDE_INFO)) {
+//            PendingIntent pendingIntent = PendingIntent.getActivity(
+//                    mContext, 0, new Intent(mContext, HideAct.class),
+//                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+//            notificationBuilder.setContentIntent(pendingIntent);
+//        } else {
+//            mHandler.removeCallbacks(mDelayRunnable);
+//            PendingIntent pendingIntent = PendingIntent.getActivity(
+//                    mContext, 0, new Intent(mContext, NoteAct.class),
+//                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+//            notificationBuilder.setContentIntent(pendingIntent);
+////            mHandler.postDelayed(mDelayRunnable, DELAY_TIME);
+//            startCountdownWithAlarm(mContext, DELAY_TIME);
+//        }
         // 更新通知内容
-        notificationBuilder.setContentTitle(content);
+        mSmallView.setTextViewText(R.id.notification_title, content);
+        notificationBuilder.setCustomContentView(mSmallView);
 
         // 使用相同的ID更新通知
         mNotificationManager.notify(1, notificationBuilder.build());
@@ -139,7 +150,8 @@ public class NotificationHelper {
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, NotificationService.class);
         intent.putExtra(HIDE_INFO, HIDE_INFO);
-        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
         alarmManager.cancel(pendingIntent);
         alarmManager.set(AlarmManager.RTC_WAKEUP,
